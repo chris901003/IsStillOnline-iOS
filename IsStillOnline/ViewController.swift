ac76305499b3f9a615c281a8e71696fd109730f7
chris901003 <a0987999103@gmail.com>
2025-01-10 11:24:05 +0800

    <IsStillOnline> # feat: new project

---
+++ IsStillOnline/ViewController.swift
@@ -0,0 +1,20 @@
+// Created for IsStillOnline in 2025
+// Using Swift 5.0
+//
+//
+// Created by HongYan on 2025/1/10.
+// Copyright © 2025 HongYan. All rights reserved.
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+
+}
+
