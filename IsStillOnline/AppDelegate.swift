a8e8d36e38046538f28c0f5142dc9a89c8caa8e1
chris901003 <a0987999103@gmail.com>
2025-01-17 22:40:33 +0800

    <IsStillOnline> #13 feat: add firebase info and message function

--- IsStillOnline/AppDelegate.swift
+++ IsStillOnline/AppDelegate.swift
@@ -7,31 +7,70 @@
 
 
 import UIKit
+import Firebase
+import FirebaseMessaging
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        FirebaseApp.configure()
+        Messaging.messaging().delegate = self
+        setupRemoteNotification()
         return true
     }
 
-    // MARK: UISceneSession Lifecycle
-
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
-
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) { }
 }
 
+// MARK: - MessagingDelegate
+extension AppDelegate: MessagingDelegate {
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        print("✅ FCM Token: \(fcmToken ?? "None")")
+    }
+}
+
+// MARK: - Request remote notification auth
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    func setupRemoteNotification() {
+        UNUserNotificationCenter.current().delegate = self
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { authenticated, error in
+            if let error {
+                print("xx-->> Error: \(error.localizedDescription)")
+            }
+            guard authenticated else { return }
+            DispatchQueue.main.async {
+                UIApplication.shared.registerForRemoteNotifications()
+            }
+        }
+    }
+
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        // Use this if you want to handle user tapping on the notification
+        let notification = response.notification
+        switch UIApplication.shared.applicationState {
+        case .active: // taps from foreground
+            print("✅ Active")
+            break
+        case .inactive: // taps from inactive state
+            print("✅ Inactive")
+            break
+        case .background: // taps from terminated state
+            print("✅ Background")
+            break
+        default:
+            print("✅ Default")
+            break
+        }
+        completionHandler()
+    }
+
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        // Use this if you want to handle notification as soon as notification was received while app is in foreground.
+        print("✅ Will present")
+        completionHandler([.badge, .banner])
+    }
+}
