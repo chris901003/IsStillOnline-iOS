24ede1e3b0a9ba49e0db6630b8dc7970046382c3
chris901003 <a0987999103@gmail.com>
2025-01-19 17:10:43 +0800

    <IsStillOnline> #15 feat: remove self login

--- IsStillOnline/Account/Login/LoginViewController.swift
+++ IsStillOnline/Account/Login/LoginViewController.swift
@@ -14,21 +14,6 @@ import AuthenticationServices
 class LoginViewController: UIViewController {
     let mainContentView = UIView()
     let titleView = UILabel()
-    let emailView = LoginInputView(config: .init(
-        title: "Email",
-        placeholder: "Email Address",
-        isPassword: false,
-        keyboardType: .emailAddress
-    ))
-    let passwordView = LoginInputView(config: .init(
-        title: "Password",
-        placeholder: "Your Password",
-        isPassword: true,
-        keyboardType: .alphabet
-    ))
-    let loginButton = UILabel()
-    let loginLoading = UIActivityIndicatorView()
-    let loginErrorMessageView = UILabel()
     let appleSignButton = ASAuthorizationAppleIDButton(type: .signIn, style: .black)
     let googleSignButton = GIDSignInButton()
 
@@ -44,28 +29,10 @@ class LoginViewController: UIViewController {
 
         view.backgroundColor = .white
 
-        titleView.text = "Account"
+        titleView.text = "Login"
         titleView.textColor = .black
         titleView.font = .systemFont(ofSize: 32, weight: .bold)
         titleView.textAlignment = .center
-
-        emailView.delegate = manager
-        passwordView.delegate = manager
-
-        loginButton.text = "Login"
-        loginButton.textColor = .black
-        loginButton.font = .systemFont(ofSize: 26, weight: .bold)
-        loginButton.isUserInteractionEnabled = true
-        loginButton.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(loginAction)))
-
-        loginLoading.color = .black
-        loginLoading.style = .large
-        loginLoading.alpha = 0
-
-        loginErrorMessageView.alpha = 0
-        loginErrorMessageView.textColor = .systemPink
-        loginErrorMessageView.font = .systemFont(ofSize: 16, weight: .semibold)
-        loginErrorMessageView.textAlignment = .center
 
         googleSignButton.style = .wide
         googleSignButton.addGestureRecognizer(UITapGestureRecognizer(target: manager, action: #selector(manager.loginWithGoogleAction)))
@@ -78,7 +45,9 @@ class LoginViewController: UIViewController {
         mainContentView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             mainContentView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            mainContentView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            mainContentView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            mainContentView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            mainContentView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
         ])
 
         mainContentView.layoutMargins = .init(top: 8, left: 8, bottom: 8, right: 0)
@@ -92,102 +61,23 @@ class LoginViewController: UIViewController {
             titleView.trailingAnchor.constraint(equalTo: layout.trailingAnchor)
         ])
 
-        mainContentView.addSubview(emailView)
-        emailView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            emailView.topAnchor.constraint(equalTo: titleView.bottomAnchor, constant: 16),
-            emailView.leadingAnchor.constraint(equalTo: layout.leadingAnchor),
-            emailView.trailingAnchor.constraint(equalTo: layout.trailingAnchor)
-        ])
-
-        mainContentView.addSubview(passwordView)
-        passwordView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            passwordView.topAnchor.constraint(equalTo: emailView.bottomAnchor, constant: 12),
-            passwordView.leadingAnchor.constraint(equalTo: layout.leadingAnchor),
-            passwordView.trailingAnchor.constraint(equalTo: layout.trailingAnchor)
-        ])
-
-        mainContentView.addSubview(loginButton)
-        loginButton.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            loginButton.topAnchor.constraint(equalTo: passwordView.bottomAnchor, constant: 32),
-            loginButton.centerXAnchor.constraint(equalTo: layout.centerXAnchor)
-        ])
-
-        mainContentView.addSubview(loginLoading)
-        loginLoading.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            loginLoading.centerXAnchor.constraint(equalTo: loginButton.centerXAnchor),
-            loginLoading.centerYAnchor.constraint(equalTo: loginButton.centerYAnchor)
-        ])
-
-        mainContentView.addSubview(loginErrorMessageView)
-        loginErrorMessageView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            loginErrorMessageView.topAnchor.constraint(equalTo: loginButton.bottomAnchor, constant: 8),
-            loginErrorMessageView.leadingAnchor.constraint(equalTo: layout.leadingAnchor),
-            loginErrorMessageView.trailingAnchor.constraint(equalTo: layout.trailingAnchor),
-            loginErrorMessageView.bottomAnchor.constraint(equalTo: layout.bottomAnchor)
-        ])
-
-        view.addSubview(appleSignButton)
+        mainContentView.addSubview(appleSignButton)
         appleSignButton.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            appleSignButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            appleSignButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
-            appleSignButton.topAnchor.constraint(equalTo: mainContentView.bottomAnchor, constant: 8),
+            appleSignButton.leadingAnchor.constraint(equalTo: mainContentView.leadingAnchor, constant: 16),
+            appleSignButton.trailingAnchor.constraint(equalTo: mainContentView.trailingAnchor, constant: -16),
+            appleSignButton.topAnchor.constraint(equalTo: titleView.bottomAnchor, constant: 16),
             appleSignButton.heightAnchor.constraint(equalToConstant: 45)
         ])
 
-        view.addSubview(googleSignButton)
+        mainContentView.addSubview(googleSignButton)
         googleSignButton.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            googleSignButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            googleSignButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
-            googleSignButton.topAnchor.constraint(equalTo: appleSignButton.bottomAnchor, constant: 8)
+            googleSignButton.leadingAnchor.constraint(equalTo: mainContentView.leadingAnchor, constant: 16),
+            googleSignButton.trailingAnchor.constraint(equalTo: mainContentView.trailingAnchor, constant: -16),
+            googleSignButton.topAnchor.constraint(equalTo: appleSignButton.bottomAnchor, constant: 8),
+            googleSignButton.bottomAnchor.constraint(equalTo: layout.bottomAnchor)
         ])
-    }
-}
-
-extension LoginViewController {
-    @objc func loginAction() {
-        var isValid = true
-        if let emailErrorMessage = manager.checkEmail() {
-            emailView.updateError(err: emailErrorMessage)
-            isValid = false
-        }
-        if let passwordErrorMessage = manager.checkPassword() {
-            passwordView.updateError(err: passwordErrorMessage)
-            isValid = false
-        }
-        guard isValid else { return }
-
-        emailView.cleanError()
-        passwordView.cleanError()
-
-        UIView.animate(withDuration: 0.1) { [weak self] in
-            guard let self else { return }
-            loginButton.alpha = 0
-            loginLoading.startAnimating()
-            loginLoading.alpha = 1
-        }
-
-        Task {
-            let result = await manager.loginAction()
-            if !result {
-                await MainActor.run {
-                    UIView.animate(withDuration: 0.25) { [weak self] in
-                        guard let self else { return }
-                        loginErrorMessageView.alpha = 1
-                        loginErrorMessageView.text = "Email or Password wrong."
-                        loginButton.alpha = 1
-                        loginLoading.alpha = 0
-                        loginLoading.stopAnimating()
-                    }
-                }
-            }
-        }
     }
 }
 
