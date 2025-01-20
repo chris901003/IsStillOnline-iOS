0fffc032192c8c026a6c615795c49b8662c5bd93
chris901003 <a0987999103@gmail.com>
2025-01-20 22:03:07 +0800

    <IsStillOnline> #20 feat: remove apple login crypto change to use library

--- IsStillOnline/Account/Login/LoginViewController+Apple.swift
+++ IsStillOnline/Account/Login/LoginViewController+Apple.swift
@@ -8,14 +8,14 @@
 
 import Foundation
 import AuthenticationServices
-import CryptoKit
 import FirebaseAuth
+import xxooooxxCommonFunction
 
 extension LoginViewController: ASAuthorizationControllerDelegate {
     static var currentNonce: String?
 
     @objc func loginWithAppleAction() {
-        LoginViewController.currentNonce = startSignInWithAppleFlow(delegate: self, presentation: self)
+        LoginViewController.currentNonce = AppleLoginFlow.startSignWithAppleFlow(delegate: self, presentation: self)
     }
 
     func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
@@ -37,43 +37,6 @@ extension LoginViewController: ASAuthorizationControllerDelegate {
     }
 
     func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) { }
-
-    func startSignInWithAppleFlow(delegate: ASAuthorizationControllerDelegate, presentation: ASAuthorizationControllerPresentationContextProviding) -> String {
-        let nonce = randomNonceString()
-        let appleIDProvider = ASAuthorizationAppleIDProvider()
-        let request = appleIDProvider.createRequest()
-        request.requestedScopes = [.fullName, .email]
-        request.nonce = sha256(nonce)
-
-        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
-        authorizationController.delegate = delegate
-        authorizationController.presentationContextProvider = presentation
-        authorizationController.performRequests()
-        return nonce
-    }
-
-    private func randomNonceString(length: Int = 32) -> String {
-        precondition(length > 0)
-        var randomBytes = [UInt8](repeating: 0, count: length)
-        let errorCode = SecRandomCopyBytes(kSecRandomDefault, randomBytes.count, &randomBytes)
-        if errorCode != errSecSuccess {
-            fatalError("Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)")
-        }
-        let charset: [Character] = Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
-        let nonce = randomBytes.map { byte in
-            charset[Int(byte) % charset.count]
-        }
-        return String(nonce)
-    }
-
-    private func sha256(_ input: String) -> String {
-        let inputData = Data(input.utf8)
-        let hashedData = SHA256.hash(data: inputData)
-        let hashString = hashedData.compactMap {
-            String(format: "%02x", $0)
-        }.joined()
-        return hashString
-    }
 }
 
 extension LoginViewController: ASAuthorizationControllerPresentationContextProviding {
